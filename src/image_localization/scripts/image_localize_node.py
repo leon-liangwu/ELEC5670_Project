3449a939adf2032ede8ccdf59480cbf15f5e643b
leon-liangwu <leon.liangwu@gmail.com>
2020-05-21 21:17:24 +0800

    start localization

--- src/image_localization/scripts/image_localize_node.py
+++ src/image_localization/scripts/image_localize_node.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # image recognization and location
 import rospy
+import numpy as np
 import cv2
 import dlib
 from sensor_msgs.msg import Image, LaserScan
@@ -11,11 +12,19 @@ face_detector = dlib.get_frontal_face_detector()
 pub1 = rospy.Publisher('vrep/image_flip', Image, queue_size=10)
 img_bridge = CvBridge()
 
+
+img_w = 512
+img_h = 512
+cam_ang = 45
+pix_ang = np.pi/4/512 
+
 last_img = None
 last_rect = None
 
-def image_callback(data):
+face_ids = []
 
+def image_callback(data):
+    global last_rect
     try:
         img = img_bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
@@ -23,7 +32,7 @@ def image_callback(data):
     
     img = cv2.flip(img, 1)
 
-    # img_resized = cv2.resize(img, (500)
+    # img_resized = cv2.resize(img, (320, 320))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # detect faces in the grayscale image
     rects = face_detector(gray, 1)
@@ -37,7 +46,7 @@ def image_callback(data):
         x1, y1 = (rect.left(), rect.top())
         x2, y2 = (rect.right(), rect.bottom())
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        rospy.loginfo(rect)
+        rospy.loginfo(last_rect)
     
     cv2.imshow('img', img)
     cv2.waitKey(10)
@@ -52,7 +61,18 @@ def image_callback(data):
 
 
 def scan_callback(data):
-    rospy.loginfo(len(data.ranges))
+    if last_rect is None:
+        return
+    
+    rect_left = last_rect.left()
+    rect_right = last_rect.right()
+    angle_range = ((rect_left-img_w/2)*pix_ang, (rect_right-img_w/2)*pix_ang)
+
+    las_ang_min = data.angle_min
+    las_ang_max = data.angle_max
+    
+    rospy.loginfo((len(data.ranges), las_ang_min, las_ang_max, angle_range))
+    # index_range = 
 
 def main():
     
