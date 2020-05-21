1c3743d1cb6988e3f27e897c21caccd0fe27897b
leon-liangwu <leon.liangwu@gmail.com>
2020-05-21 19:02:34 +0800

    start 3

---
+++ src/image_localization/scripts/image_localize_node.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+# image recognization and location
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
--- src/image_location/image_loacte.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# image recognization and location
-import rospy
-import cv2
-from sensor_msgs.msg import Image
-
-
